15a2e6d85fcaadca5b5cdcdedc44f557c3a1c51c
galzada <galzada570@gmail.com>
2026-07-06 15:47:50 +0300

    Implement hello API and connect React client

--- server/obj/Debug/net10.0/server.AssemblyInfo.cs
+++ server/obj/Debug/net10.0/server.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("server")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+505c1974fa8aeeb7fae259d51778576b158376c8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b8ab9aeff676c56adeebef881f0cb6fbcee9c729")]
 [assembly: System.Reflection.AssemblyProductAttribute("server")]
 [assembly: System.Reflection.AssemblyTitleAttribute("server")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
