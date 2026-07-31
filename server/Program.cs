4206f6bb0f9b2fa272d5f44bba705506c4ba02f0
galzada <galzada570@gmail.com>
2026-07-31 15:38:36 +0300

    add SQlite persistence for trips data

--- server/Program.cs
+++ server/Program.cs
@@ -1,7 +1,12 @@
+using Microsoft.EntityFrameworkCore;
+using server.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddDbContext<TripPlannerContext>(options =>
+    options.UseSqlite(builder.Configuration.GetConnectionString("TripPlanner")));
 
 builder.Services.AddCors(options =>
 {
@@ -17,6 +22,12 @@ builder.Services.AddCors(options =>
 var app = builder.Build();
 
 app.UseCors();
+
+using (var scope = app.Services.CreateScope())
+{
+    var database = scope.ServiceProvider.GetRequiredService<TripPlannerContext>();
+    database.Database.EnsureCreated();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
